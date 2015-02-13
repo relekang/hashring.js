2c61f2ed66e6267ec7460a256e859e8cca9bc140
Rolf Erik Lekang <me@rolflekang.com>
2015-02-13 15:35:43 +0100

    Add browser run of the mocha test suite on CI

---
+++ karma.conf.js
@@ -0,0 +1,15 @@
+module.exports = function(config) {
+  config.set({
+    frameworks: ['browserify', 'mocha'],
+    files: [
+      'test/*.js'
+    ],
+    preprocessors: {
+      'test/*.js': ['browserify'],
+    },
+    browsers: [
+      'PhantomJS',
+    ],
+    singleRun: false
+  });
+};
